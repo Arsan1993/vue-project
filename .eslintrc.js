8ad212da3d03b206cac38601fa089e06aa3e7b1e
Arsan <arsanxx@mail.ru>
2023-03-29 14:02:56 +0300

    first step of vue-project

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'max-len': ['error', { code: 120 }],
+  },
+
+};
