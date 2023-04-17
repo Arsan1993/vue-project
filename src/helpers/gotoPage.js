abdd5aeb6c3072f9ddef2c2f19601be773966156
Arsan Khadzhimuradov <arsanxx@mail.ru>
2023-04-17 22:07:34 +0300

    added html

--- src/helpers/gotoPage.js
+++ src/helpers/gotoPage.js
@@ -1,5 +1,6 @@
-import eventBus from '@/eventBus';
+/* import eventBus from '@/eventBus';
 
 export default function gotoPage(pageName, pageParams) {
   eventBus.$emit('gotoPage', pageName, pageParams);
 }
+ */
